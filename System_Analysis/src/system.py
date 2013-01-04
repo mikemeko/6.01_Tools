4268a6c6243c8d4b94798b072ca127d519590f29
Michael Mekonnen <mikemeko@mit.edu>
2013-01-03 21:11:09 -0500

    first pass at adder

--- System_Analysis/src/system.py
+++ System_Analysis/src/system.py
@@ -27,14 +27,21 @@ class Gain(Component):
     Component.__init__(self, inp, out)
     self.K = K
   def update(self, inp_bound):
-    self.new_out = inp_bound.scalar_mult(self.K)
+    self.new_out = inp_bound[0].scalar_mult(self.K)
 
 class Delay(Component):
   """
   TODO(mikemeko)
   """
   def update(self, inp_bound):
-    self.new_out = inp_bound.shift()
+    self.new_out = inp_bound[0].shift()
+
+class Adder(Component):
+  """
+  TODO(mikemeko)
+  """
+  def update(self, inp_bound):
+    self.new_out = reduce(lambda p_1, p_2: p_1 + p_2, inp_bound)
 
 def solve_system(components, last_comp, num_variables):
   """
@@ -43,16 +50,16 @@ def solve_system(components, last_comp, num_variables):
   covered_variables = {'X':Polynomial({'X':R_Polynomial([1])}),
       'Y':Polynomial({'Y':R_Polynomial([1])})}
   while len(covered_variables) < num_variables:
-    for comp in components:
-      if comp.new_out is None and comp.inp in covered_variables:
-        comp.update(covered_variables[comp.inp])
-        covered_variables[comp.out] = comp.new_out
+    for c in components:
+      if c.new_out is None and all(i in covered_variables for i in c.inp):
+        c.update([covered_variables[i] for i in c.inp])
+        covered_variables[c.out] = c.new_out
         break
-  last_comp.update(covered_variables[last_comp.inp])
+  last_comp.update([covered_variables[i] for i in last_comp.inp])
   return covered_variables[last_comp.out] - last_comp.new_out
 
 if __name__ == '__main__':
-  comp_1 = Gain('X', 'A', 2)
-  comp_2 = Delay('A', 'Y')
+  comp_1 = Delay(['A'], 'Y')
+  comp_2 = Adder(['X','Y'], 'A')
   components = [comp_1, comp_2]
-  print solve_system(components, comp_2, 3)
+  print solve_system(components, comp_1, 3)
