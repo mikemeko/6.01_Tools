b081fa3a1889a840f068cf622c24534ff0a387ad
Michael Mekonnen <mikemeko@mit.edu>
2013-01-09 20:14:40 -0500

    show pole-zero and unit sample response at the same time instead of one after the other

--- System_Analysis/src/main.py
+++ System_Analysis/src/main.py
@@ -10,11 +10,12 @@ from core.system import Delay
 from core.system import Gain
 from core.system import System
 from core.unit_sample_response import plot_unit_sample_response
+from threading import Thread
 
 if __name__ == '__main__':
   # test system
   sys = System([Adder(['X', 'E'], 'A'), Gain('D', 'E', -1),
       Gain('A', 'B', 0.1), Adder(['B', 'Y'], 'C'), Delay('C', 'Y'),
       Delay('Y', 'D')])
-  plot_pole_zero_diagram(sys)
-  plot_unit_sample_response(sys)
+  Thread(target=plot_pole_zero_diagram, args=(sys,)).start()
+  Thread(target=plot_unit_sample_response, args=(sys,)).start()
