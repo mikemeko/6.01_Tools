ab9fb70ec7ea38cabdc1f47c26624005eff8b6aa
Michael Mekonnen <mikemeko@mit.edu>
2013-01-19 17:04:13 -0500

    cleaned up example.py

--- System_Analysis/src/gui/example.py
+++ System_Analysis/src/gui/example.py
@@ -13,43 +13,88 @@ from constants import CONNECTOR_TOP
 from Tkinter import Tk
 
 class Circle(Drawable):
-  def __init__(self, x, y, radius, connectors, fill):
-    Drawable.__init__(self, (x - radius, y - radius, x + radius, y + radius),
-        connectors)
-    self.fill = fill
+  """
+  Colorful circle.
+  """
+  def __init__(self, x, y, r, connectors, color1, color2, color3, color4):
+    """
+    |x|, |y|, |r|: center and radius for the circle.
+    |color1|, |color2|, |color3|, |color4|: colors to display on the circle.
+    """
+    Drawable.__init__(self, (x - r, y - r, x + r, y + r), connectors)
+    self.x = x
+    self.y = y
+    self.r = r
+    self.color1 = color1
+    self.color2 = color2
+    self.color3 = color3
+    self.color4 = color4
   def draw_on(self, canvas):
-    x1, y1, x2, y2 = self.bounding_box
-    self.parts.add(canvas.create_oval(x1, y1, x2, y2, fill=self.fill))
+    """
+    Draws this colorful circle on the |canvas|.
+    """
+    x, y, r = self.x, self.y, self.r
+    self.parts.add(canvas.create_arc(x - r, y - r, x + r, y + r,
+        start=0, fill=self.color1))
+    self.parts.add(canvas.create_arc(x - r, y - r, x + r, y + r,
+        start=90, fill=self.color2))
+    self.parts.add(canvas.create_arc(x - r, y - r, x + r, y + r,
+        start=180, fill=self.color3))
+    self.parts.add(canvas.create_arc(x - r, y - r, x + r, y + r,
+        start=270, fill=self.color4))
 
 class Triangle(Drawable):
+  """
+  Triangle.
+  """
   def __init__(self, x1, y1, x2, y2, x3, y3, connectors, fill,
       outline='black'):
+    """
+    |x1|, |y1|, |x2|, |y2|, |x3|, |y3|: vertices of the triangle.
+    |fill|: fill color for the triangle.
+    |outline|: outline color for the triangle.
+    """
     Drawable.__init__(self, (min(x1, x2, x3), min(y1, y2, y3), max(x1, x2, x3),
         max(y1, y2, y3)), connectors)
-    self.x1, self.y1, self.x2, self.y2, self.x3, self.y3 = (x1, y1, x2, y2, x3,
-        y3)
+    self.x1, self.y1 = x1, y1
+    self.x2, self.y2 = x2, y2
+    self.x3, self.y3 = x3, y3
     self.fill = fill
     self.outline = outline
   def draw_on(self, canvas):
+    """
+    Draws this triangle on the |canvas|.
+    """
     self.parts.add(canvas.create_polygon(self.x1, self.y1, self.x2, self.y2,
         self.x3, self.y3, fill=self.fill, outline=self.outline))
 
 class Rectangle(Drawable):
+  """
+  Rectangle.
+  """
   def __init__(self, x1, y1, x2, y2, connectors, fill):
+    """
+    |x1|, |y1|, |x2|, |y2|: bounding box for the rectangle.
+    |fill|: fill color for the rectangle.
+    """
     Drawable.__init__(self, (x1, y1, x2, y2), connectors)
     self.fill = fill
   def draw_on(self, canvas):
-    self.parts.add(canvas.create_rectangle(self.bounding_box,
-        fill=self.fill))
+    """
+    Draws this rectangle on the |canvas|.
+    """
+    self.parts.add(canvas.create_rectangle(self.bounding_box, fill=self.fill))
 
 if __name__ == '__main__':
   root = Tk()
   root.resizable(0, 0)
+  # create board
   board = Board(root)
+  # add items to the board
   board.add_drawable(Triangle(100, 100, 100, 200, 150, 150, CONNECTOR_LEFT |
       CONNECTOR_RIGHT, 'green'))
-  board.add_drawable(Circle(300, 300, 20, CONNECTOR_TOP | CONNECTOR_BOTTOM,
-      'yellow'))
-  board.add_drawable(Rectangle(10, 10, 70, 70, CONNECTOR_LEFT | CONNECTOR_RIGHT |
-      CONNECTOR_TOP | CONNECTOR_BOTTOM, 'blue'))
+  board.add_drawable(Circle(300, 300, 20, CONNECTOR_TOP | CONNECTOR_BOTTOM |
+      CONNECTOR_LEFT | CONNECTOR_RIGHT, 'yellow', 'red', 'orange', 'cyan'))
+  board.add_drawable(Rectangle(10, 10, 70, 70, CONNECTOR_TOP |
+      CONNECTOR_BOTTOM, 'blue'))
   board.mainloop()
