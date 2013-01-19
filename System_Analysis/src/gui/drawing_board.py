592278e7ce3174ac6b5b427cff4c9932c73be69e
Michael Mekonnen <mikemeko@mit.edu>
2013-01-19 12:11:37 -0500

    more sample drawables

--- System_Analysis/src/gui/drawing_board.py
+++ System_Analysis/src/gui/drawing_board.py
@@ -238,12 +238,33 @@ class Circle(Drawable):
     x1, y1, x2, y2 = self.bounding_box
     self.parts.append(canvas.create_oval(x1, y1, x2, y2, fill=self.fill))
 
+class Triangle(Drawable):
+  def __init__(self, x1, y1, x2, y2, x3, y3, connectors, fill):
+    Drawable.__init__(self, (min(x1, x2, x3), min(y1, y2, y3), max(x1, x2, x3),
+        max(y1, y2, y3)), connectors)
+    self.x1, self.y1, self.x2, self.y2, self.x3, self.y3 = (x1, y1, x2, y2, x3,
+        y3)
+    self.fill = fill
+  def draw_on(self, canvas):
+    self.parts.append(canvas.create_polygon(self.x1, self.y1, self.x2, self.y2,
+        self.x3, self.y3, fill=self.fill))
+
+class Rectangle(Drawable):
+  def __init__(self, x1, y1, x2, y2, connectors, fill):
+    Drawable.__init__(self, (x1, y1, x2, y2), connectors)
+    self.fill = fill
+  def draw_on(self, canvas):
+    self.parts.append(canvas.create_rectangle(self.bounding_box,
+        fill=self.fill))
+
 if __name__ == '__main__':
   root = Tk()
   root.resizable(0, 0)
   board = Drawing_Board(root)
-  board.add_drawable(Circle(100, 100, 10, CONNECTOR_BOTTOM | CONNECTOR_LEFT |
-      CONNECTOR_RIGHT, 'yellow'))
-  board.add_drawable(Circle(200, 200, 20, CONNECTOR_LEFT | CONNECTOR_RIGHT,
-      'red'))
+  board.add_drawable(Triangle(100, 100, 100, 200, 150, 150, CONNECTOR_LEFT |
+      CONNECTOR_RIGHT, 'green'))
+  board.add_drawable(Circle(300, 300, 20, CONNECTOR_TOP | CONNECTOR_BOTTOM,
+      'yellow'))
+  board.add_drawable(Rectangle(10, 10, 70, 70, CONNECTOR_LEFT | CONNECTOR_RIGHT |
+      CONNECTOR_TOP | CONNECTOR_BOTTOM, 'blue'))
   board.mainloop()
