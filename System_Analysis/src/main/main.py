72d7275a45a3fa53ad06fe2ed7f9df63db5c9e24
Michael Mekonnen <mikemeko@mit.edu>
2013-02-03 12:57:30 -0500

    Ctrl-x shortcuts; menu for analysis.

--- System_Analysis/src/main/main.py
+++ System_Analysis/src/main/main.py
@@ -136,15 +136,22 @@ if __name__ == '__main__':
   board.add_key_binding('s', save_file, CTRL_DOWN)
   board.add_key_binding('o', open_file, CTRL_DOWN)
   board.add_key_binding('n', new_file, CTRL_DOWN)
+  board.add_key_binding('q', board.quit, CTRL_DOWN)
   # menu
   menu = Menu(root, tearoff=0)
   file_menu = Menu(menu, tearoff=0)
-  file_menu.add_command(label='New', command=new_file)
-  file_menu.add_command(label='Open', command=open_file)
-  file_menu.add_command(label='Save', command=save_file)
+  file_menu.add_command(label='New', command=new_file, accelerator='Ctrl+N')
+  file_menu.add_command(label='Open', command=open_file, accelerator='Ctrl+O')
+  file_menu.add_command(label='Save', command=save_file, accelerator='Ctrl+S')
   file_menu.add_separator()
-  file_menu.add_command(label='Quit', command=board.quit)
+  file_menu.add_command(label='Quit', command=board.quit, accelerator='Ctrl+Q')
   menu.add_cascade(label='File', menu=file_menu)
+  analyze_menu = Menu(menu, tearoff=0)
+  analyze_menu.add_command(label='PZD', command=lambda: run_analysis(board,
+      plot_pole_zero_diagram), accelerator='P')
+  analyze_menu.add_command(label='USR', command=lambda: run_analysis(board,
+      plot_unit_sample_response), accelerator='U')
+  menu.add_cascade(label='Analyze', menu=analyze_menu)
   root.config(menu=menu)
   # some UI help
   board.display_message(INIT_UI_HELP, INFO)
