159dba6b424605eed0a7daf5ce1e7becac197d09
Michael Mekonnen <mikemeko@mit.edu>
2013-02-08 14:59:34 -0500

    first pass at equation solver

--- src/core/math/equation_solver.py
+++ src/core/math/equation_solver.py
@@ -3,3 +3,27 @@ TODO(mikemeko)
 """
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
+
+from numpy import matrix
+from numpy.linalg import solve
+
+def solve_equations(equations):
+  """
+  TODO(mikemeko)
+  """
+  var_list = list(reduce(set.union, (set(var for coeff, var in eqn if var)
+      for eqn in equations)))
+  num_vars = len(var_list)
+  var_index = {var_list[i]: i for i in xrange(num_vars)}
+  A, b = [], []
+  for equation in equations:
+    coeffs, const = [0] * num_vars, 0
+    for coeff, var in equation:
+      if var:
+        coeffs[var_index[var]] += coeff
+      else:
+        const += coeff
+    A.append(coeffs)
+    b.append([const])
+  x = solve(matrix(A), matrix(b))
+  return {var_list[i]: x[i, 0] for i in xrange(num_vars)}
