d44c01835863a02d255d930731f95b7ab46271e2
Michael Mekonnen <mikemeko@mit.edu>
2013-02-12 16:03:51 -0500

    cleaned up circuit_simulator/simulation/circuit.py

--- src/circuit_simulator/simulation/circuit.py
+++ src/circuit_simulator/simulation/circuit.py
@@ -1,6 +1,6 @@
 """
-TODO(mikemeko)
-Remember to acknowledge software lab 9.
+Representation for circuit components and circuits.
+Credit to ideas from MIT 6.01 Fall 2012 Software Lab 9.
 """
 
 from core.math.equation_solver import solve_equations
@@ -9,11 +9,14 @@ __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
 class One_Port:
   """
-  TODO(mikemeko)
+  Representation for a circuit component across which a voltage difference
+      develops and through which a current flows.
   """
   def __init__(self, n1, n2, i):
     """
-    TODO(mikemeko)
+    |n1|: first (+) node this one port is connected to.
+    |n2|: second (-) node this one port is connected to.
+    |i|: the current through this one port, flowing from |n1| to |n2|.
     """
     self.n1 = n1
     self.n2 = n2
@@ -21,66 +24,76 @@ class One_Port:
   @property
   def equation(self):
     """
-    TODO(mikemeko)
+    Returns an equation representing the constraint that needs to be satisfied
+        for this one port. An equation should be given in the form of a list of
+        terms summing to 0, where a term is a tuple of the form (coeff, var),
+        where coeff is a number and var is a variable. Constants can be
+        represented by (const, None).
     """
     raise Exception('subclasses should implement this')
 
 class Voltage_Source(One_Port):
   """
-  TODO(mikemeko)
+  Representation for voltage source component.
   """
   def __init__(self, n1, n2, i, v0):
     """
-    TODO(mikemeko)
+    |v0|: the voltage difference (|n1| - |n2|) this voltage source provides.
     """
     One_Port.__init__(self, n1, n2, i)
     self.v0 = v0
   def equation(self):
+    # n1 - n0 = v0
     return [(1, self.n1), (-1, self.n2), (-self.v0, None)]
-  def __str__(self):
-    return 'Vsrc %s %s %s %f' % (self.n1, self.n2, self.i, self.v0)
 
 class Resistor(One_Port):
   """
-  TODO(mikemeko)
+  Representation for resistor component.
   """
   def __init__(self, n1, n2, i, r):
     """
-    TODO(mikemeko)
+    |r|: resistance (impedance) of this resistor.
     """
     One_Port.__init__(self, n1, n2, i)
     self.r = r
   def equation(self):
+    # n1 - n2 = ir
     return [(1, self.n1), (-1, self.n2), (-self.r, self.i)]
-  def __str__(self):
-    return 'Resistor %s %s %s %f' % (self.n1, self.n2, self.i, self.r)
 
 class Circuit:
   """
-  TODO(mikemeko)
+  Representation for a circuit.
   """
   def __init__(self, components, gnd):
     """
-    TODO(mikemeko)
+    |components|: a list of the components (one ports) in this circuit.
+    |gnd|: the ground node in this circuit.
     """
     assert all(isinstance(c, One_Port) for c in components), ('all components '
         'must be one ports')
     self.components = components
     self.gnd = gnd
-    self.data = None # TODO(mikemeko): clarify
-    self._solve()
+    # self.data contains all values of the currents and voltages in this
+    #     circuit, if solved correctly, None otherwise
+    self.data = self._solve()
   def _solve(self):
-    equations = []
+    """
+    Solves this circuit and returns a dictionary mapping all the variables (
+        i.e. voltages and currents) to their respective values. If circuit
+        cannot be solved, returns None.
+    """
+    # accumulate and solve system of equations
+    # component equations
+    equations = [component.equation() for component in self.components]
+    # one KCL equation per node in the circuit (excluding ground node)
     KCL = {}
     for component in self.components:
-      equations.append(component.equation())
       KCL[component.n1] = KCL.get(component.n1, []) + [(1, component.i)]
       KCL[component.n2] = KCL.get(component.n2, []) + [(-1, component.i)]
     equations.extend([KCL[n] for n in KCL if n is not self.gnd])
+    # assert that ground voltage is 0
     equations.append([(1, self.gnd)])
-    self.data = solve_equations(equations)
-
-if __name__ == '__main__':
-  c = Circuit([Voltage_Source('e1', 'e0', 'i3', 10), Resistor('e1', 'e2', 'i1',
-      3), Resistor('e2', 'e0', 'i2', 3)], 'e0')
-  print c.data
+    try:
+      return solve_equations(equations)
+    except:
+      return None
