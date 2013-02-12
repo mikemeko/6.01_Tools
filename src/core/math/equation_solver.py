c5217c51bdfb1ff0615ae896c4a908cd84eb0990
Michael Mekonnen <mikemeko@mit.edu>
2013-02-12 01:33:55 -0500

    cleaned up equation solver + unittests

--- src/core/math/equation_solver.py
+++ src/core/math/equation_solver.py
@@ -1,5 +1,5 @@
 """
-TODO(mikemeko)
+Solver for linear systems of equations.
 """
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
@@ -9,13 +9,24 @@ from numpy.linalg import solve
 
 def solve_equations(equations):
   """
-  TODO(mikemeko)
+  Solves the given system of |equations|. |equations| should be a list
+      of lists of terms summing to 0. Each term should be a tuple of the form
+      (coeff, var), where coeff is a number and var is a variable (string).
+      Constants can be represented by (const, None).
+  Returns a dictionary mapping the variables in the equations to their
+      their respective values, or raises an Exception if the system cannot be
+      solved.
   """
+  # variables in the system of equations
   var_list = list(reduce(set.union, (set(var for coeff, var in eqn if var)
       for eqn in equations)))
+  # number of variables
   num_vars = len(var_list)
+  # the index of each variable in |var_list|
   var_index = {var_list[i]: i for i in xrange(num_vars)}
+  # matrices to solve system (Ax = b)
   A, b = [], []
+  # populate matrices
   for equation in equations:
     coeffs, const = [0] * num_vars, 0
     for coeff, var in equation:
@@ -25,5 +36,9 @@ def solve_equations(equations):
         const -= coeff
     A.append(coeffs)
     b.append([const])
-  x = solve(matrix(A), matrix(b))
-  return {var_list[i]: x[i, 0] for i in xrange(num_vars)}
+  try:
+    # solve system
+    x = solve(matrix(A), matrix(b))
+    return {var_list[i]: x[i, 0] for i in xrange(num_vars)}
+  except:
+    raise Exception('Could not solve system of equations')
--- src/tests/core/math/equation_solver_test.py
+++ src/tests/core/math/equation_solver_test.py
@@ -4,4 +4,27 @@ Unittests for equation_solver.py.
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
-# TODO(mikemeko): write tests :)
+from core.math.equation_solver import solve_equations
+from unittest import main
+from unittest import TestCase
+
+class Equation_Solver_Test(TestCase):
+  """
+  Tests for gui/math/equation_solver.
+  """
+  def test_simple_equation(self):
+    solution = solve_equations([[(1, 'x')]])
+    self.assertDictEqual(solution, {'x': 0})
+  def test_one_equation(self):
+    solution = solve_equations([[(2, 'x'), (4, None)]])
+    self.assertDictEqual(solution, {'x': -2})
+  def test_two_equations(self):
+    eq_1 = [(5, 'x'), (-2, 'y'), (-3, None)]
+    eq_2 = [(3, 'x'), (4, 'y'), (-33, None)]
+    solution = solve_equations([eq_1, eq_2])
+    self.assertDictEqual(solution, {'x': 3, 'y': 6})
+  def test_fail(self):
+    self.assertRaises(Exception, solve_equations, [[(1, 'x'), (1, 'y')]])
+
+if __name__ == '__main__':
+  main()
