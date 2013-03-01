63a00825e5d6800de0eb3d5cafd838b1a52c54ef
Michael Mekonnen <mikemeko@mit.edu>
2013-02-28 19:39:29 -0500

    unittests for undo

--- src/tests/core/util/undo_test.py
+++ src/tests/core/util/undo_test.py
@@ -1,5 +1,6 @@
 """
 Unittests for undo.py.
+TODO(mikemeko): better test for Multi_Action.
 """
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonne)'
@@ -14,6 +15,61 @@ class Undo_Test(TestCase):
   """
   Tests for core/util/undo.py.
   """
+  def setUp(self):
+    self._action_history = Action_History()
+    self._A = self._create_action('A')
+    self._B = self._create_action('B')
+    self._actions = []
+  def _create_action(self, val):
+    def do_action():
+      self._actions.append(val)
+    def undo_action():
+      self._actions.pop()
+    return Action(do_action, undo_action)
+  def _last_action(self):
+    return self._actions[-1]
+  def _do(self, action):
+    action.do_action()
+    self._action_history.record_action(action)
+  def _undo(self):
+    return self._action_history.undo()
+  def _redo(self):
+    return self._action_history.redo()
+  def _clear(self):
+    self._action_history.clear()
+  def test_history_init(self):
+    self.assertFalse(self._undo())
+    self.assertFalse(self._redo())
+  def test_undo_redo(self):
+    self._do(self._A)
+    self.assertEqual('A', self._last_action())
+    self._do(self._B)
+    self.assertEqual('B', self._last_action())
+    self.assertTrue(self._undo())
+    self.assertEqual('A', self._last_action())
+    self.assertTrue(self._undo())
+    self.assertFalse(self._actions)
+    self.assertFalse(self._undo())
+    self.assertTrue(self._redo())
+    self.assertEqual('A', self._last_action())
+    self.assertTrue(self._redo())
+    self.assertEqual('B', self._last_action())
+    self.assertFalse(self._redo())
+  def test_clear(self):
+    self._do(self._A)
+    self._do(self._B)
+    self._clear()
+    self.assertFalse(self._undo())
+    self.assertFalse(self._redo())
+  def test_multi_action(self):
+    multi_action = Multi_Action([self._A, self._B])
+    self._do(multi_action)
+    self.assertEqual('B', self._last_action())
+    self.assertTrue(self._undo())
+    self.assertFalse(self._actions)
+    self.assertTrue(self._redo())
+    self.assertEqual('B', self._last_action())
+    self.assertFalse(self._redo())
 
 if __name__ == '__main__':
   main()
