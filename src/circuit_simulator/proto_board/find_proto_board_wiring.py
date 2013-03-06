de59c586381d8421b8f7d6fb4bb566ee4f83c664
Michael Mekonnen <mikemeko@mit.edu>
2013-03-06 02:32:41 -0500

    work in progress: some clean up

--- src/circuit_simulator/proto_board/find_proto_board_wiring.py
+++ src/circuit_simulator/proto_board/find_proto_board_wiring.py
@@ -12,24 +12,21 @@ from proto_board import Proto_Board
 # currently assumes that no pwr gnd rail locations
 class Proto_Board_Search_Node(Search_Node):
   def __init__(self, proto_board, loc_pairs, current_wires, parent=None):
+    assert isinstance(proto_board, Proto_Board)
+    for loc_1, loc_2 in loc_pairs:
+      assert proto_board.valid_loc(loc_1)
+      assert proto_board.valid_loc(loc_2)
     Search_Node.__init__(self, (proto_board, loc_pairs, current_wires), parent)
-  def _get_new_node(self, proto_board, loc_pairs, current_wires, new_wire, i):
-    loc_1, loc_2 = new_wire
-    new_proto_board = proto_board.with_wire(loc_1, loc_2)
-    new_current_wires = list(current_wires)
-    new_current_wires[i] = tuple(list(new_current_wires[i]) + [new_wire])
-    return Proto_Board_Search_Node(new_proto_board, loc_pairs,
-        tuple(new_current_wires), self)
   def get_children(self):
     children = []
     proto_board, loc_pairs, current_wires = self.state
     for i, (loc_1, loc_2) in enumerate(loc_pairs):
       if not proto_board.connected(loc_1, loc_2):
-        last_loc = current_wires[i][-1][1] if current_wires[i] else loc_2
-        for neighbor_loc in proto_board.neighbor_locs(last_loc):
+        last_loc = current_wires[i][-1][1] if current_wires[i] else loc_1
+        for neighbor_loc in proto_board.section_locs(last_loc):
           if not proto_board.occupied(neighbor_loc):
-            new_wire_ends = []
             r, c = neighbor_loc
+            new_wire_ends = []
             for l in range(1, WIRE_LENGTH_LIMIT):
               new_wire_ends.append((r, c - l))
               new_wire_ends.append((r, c + l))
@@ -39,8 +36,13 @@ class Proto_Board_Search_Node(Search_Node):
               if proto_board.valid_loc(new_wire_end) and (not
                   proto_board.occupied(new_wire_end)):
                 new_wire = (neighbor_loc, new_wire_end)
-                children.append(self._get_new_node(proto_board, loc_pairs,
-                    current_wires, new_wire, i))
+                new_proto_board = proto_board.with_wire(neighbor_loc,
+                    new_wire_end)
+                new_current_wires = list(current_wires)
+                new_current_wires[i] = tuple(list(new_current_wires[i]) +
+                    [new_wire])
+                children.append(Proto_Board_Search_Node(new_proto_board,
+                    loc_pairs, tuple(new_current_wires), self))
     return children
 
 def goal_test(state):
@@ -53,4 +55,4 @@ def find_wiring(loc_pairs, start_proto_board=Proto_Board()):
   return a_star(start_node, goal_test)
 
 if __name__ == '__main__':
-  print find_wiring((((2, 0), (2, 2)),))
+  print find_wiring((((2, 0), (5, 10)),))[0]
--- src/circuit_simulator/proto_board/proto_board.py
+++ src/circuit_simulator/proto_board/proto_board.py
@@ -5,39 +5,54 @@ TODO(mikemeko)
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
 from constants import BOTTOM_SECTION
+from constants import PROTO_BOARD_HEIGHT
 from constants import PROTO_BOARD_WIDTH
 from constants import TOP_SECTION
 from copy import deepcopy
 
 # currently assumes that no pwr gnd rail locations
 class Proto_Board:
-  def __init__(self, wires={}):
+  def __init__(self, wire_mappings={}, wires=()):
+    self._wire_mappings = wire_mappings
     self._wires = wires
+  def get_wires(self):
+    return self._wires
+  def _cross(self, wire_1, wire_2):
+    (x11, y11), (x12, y12) = wire_1
+    (x21, y21), (x22, y22) = wire_2
+    # TODO
+  def crossing_wires(self):
+    num_wires = len(self._wires)
+    return any(self._cross(self._wires[i], self._wires[j]) for i in
+        xrange(num_wires) for j in xrange(i + 1, num_wires))
   def _connected(self, loc_1, loc_2, visited=set()):
     if loc_1 in visited:
       return False
-    r, c = loc_1
-    group = set((new_r, c) for new_r in self.section_rows(r))
-    group_links = set(self._wires[loc] for loc in group if loc in self._wires)
+    group = set(self.section_locs(loc_1))
+    group_links = set(self._wire_mappings[loc] for loc in group
+        if loc in self._wire_mappings)
     return loc_2 in group or any(self._connected(new_loc_1, loc_2,
         visited | group) for new_loc_1 in group_links)
   def connected(self, loc_1, loc_2):
     return self._connected(loc_1, loc_2)
-  def neighbor_locs(self, loc):
+  def section_locs(self, loc):
     r, c = loc
-    return ((new_r, c) for new_r in (self.section_rows(r) - set([r])))
+    return ((new_r, c) for new_r in self.section_rows(r))
   def with_wire(self, loc_1, loc_2):
     assert not self.occupied(loc_1)
     assert not self.occupied(loc_2)
-    new_wires = deepcopy(self._wires)
-    new_wires[loc_1] = loc_2
-    new_wires[loc_2] = loc_1
-    return Proto_Board(new_wires)
+    new_wire_mappings = deepcopy(self._wire_mappings)
+    new_wire_mappings[loc_1] = loc_2
+    new_wire_mappings[loc_2] = loc_1
+    new_wires = list(self._wires)
+    new_wires.append((loc_1, loc_2))
+    new_wires = tuple(new_wires)
+    return Proto_Board(new_wire_mappings, new_wires)
   def occupied(self, loc):
-    return loc in self._wires
+    return loc in self._wire_mappings
   def valid_loc(self, loc):
     r, c = loc
-    return 0 <= c < PROTO_BOARD_WIDTH
+    return 0 <= r < PROTO_BOARD_HEIGHT and 0 <= c < PROTO_BOARD_WIDTH
   def section_rows(self, r):
     return BOTTOM_SECTION if r in BOTTOM_SECTION else TOP_SECTION
   def opp_section_rows(self, r):
