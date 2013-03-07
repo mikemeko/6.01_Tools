807575fb5fd8471f099e13beb41bcca3ed80be34
Michael Mekonnen <mikemeko@mit.edu>
2013-03-06 23:01:26 -0500

    proto board visualization initial commit

---
+++ src/circuit_simulator/proto_board/visualization/constants.py
@@ -0,0 +1,5 @@
+"""
+Proto board visualization constants.
+"""
+
+__author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
---
+++ src/circuit_simulator/proto_board/visualization/visualization.py
@@ -0,0 +1,14 @@
+"""
+TODO(mikemeko)
+"""
+
+__author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
+
+from Tkinter import Frame
+
+class Proto_Board_Visualization(Frame):
+  """
+  TODO(mikemeko)
+  """
+  def __init__(self):
+    pass
