e7d74dbac5815671ba57be8ab163924de4f9c1db
Michael Mekonnen <mikemeko@mit.edu>
2013-03-06 23:33:39 -0500

    empty proto board visualization

--- src/circuit_simulator/proto_board/visualization/constants.py
+++ src/circuit_simulator/proto_board/visualization/constants.py
@@ -3,3 +3,19 @@ Proto board visualization constants.
 """
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
+
+from circuit_simulator.proto_board.constants import PROTO_BOARD_HEIGHT
+from circuit_simulator.proto_board.constants import PROTO_BOARD_WIDTH
+
+# color
+BACKGROUND = 'white'
+
+# size
+CONNECTOR_SIZE = 4
+CONNECTOR_SPACING = 8
+VERTICAL_SEPARATION = 40
+PADDING = 10
+HEIGHT = (PROTO_BOARD_HEIGHT * CONNECTOR_SIZE + (PROTO_BOARD_HEIGHT - 4) *
+    CONNECTOR_SPACING + 3 * VERTICAL_SEPARATION + 2 * PADDING)
+WIDTH = (PROTO_BOARD_WIDTH * CONNECTOR_SIZE + (PROTO_BOARD_WIDTH - 1) *
+    CONNECTOR_SPACING + 2 * PADDING)
--- src/circuit_simulator/proto_board/visualization/visualization.py
+++ src/circuit_simulator/proto_board/visualization/visualization.py
@@ -4,11 +4,27 @@ TODO(mikemeko)
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
+from constants import BACKGROUND
+from constants import HEIGHT
+from constants import WIDTH
+from Tkinter import Canvas
 from Tkinter import Frame
+from Tkinter import mainloop
+from Tkinter import Tk
 
 class Proto_Board_Visualization(Frame):
   """
   TODO(mikemeko)
   """
-  def __init__(self):
-    pass
+  def __init__(self, parent):
+    Frame.__init__(self, parent, background=BACKGROUND)
+    self.canvas = Canvas(self, width=WIDTH, height=HEIGHT)
+    self._set_up()
+  def _set_up(self):
+    self.canvas.pack()
+    self.pack()
+
+if __name__ == '__main__':
+  root = Tk()
+  vis = Proto_Board_Visualization(root)
+  mainloop()
