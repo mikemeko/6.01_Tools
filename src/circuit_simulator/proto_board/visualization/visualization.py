b74feac8e7605d78595c4b7a6e79f558db2ea1dd
Michael Mekonnen <mikemeko@mit.edu>
2013-03-10 01:54:35 -0500

    cleaned up proto board visualization

--- src/circuit_simulator/proto_board/visualization/visualization.py
+++ src/circuit_simulator/proto_board/visualization/visualization.py
@@ -1,5 +1,5 @@
 """
-TODO(mikemeko)
+Proto board visualization.
 """
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
@@ -16,6 +16,7 @@ from constants import HEIGHT
 from constants import PADDING
 from constants import VERTICAL_SEPARATION
 from constants import WIDTH
+from constants import WINDOW_TITLE
 from constants import WIRE_COLOR
 from constants import WIRE_OUTLINE
 from Tkinter import Canvas
@@ -25,24 +26,39 @@ from Tkinter import Tk
 
 class Proto_Board_Visualizer(Frame):
   """
-  TODO(mikemeko)
+  Tk Frame to visualize Proto boards.
   """
   def __init__(self, wires, parent=Tk()):
+    """
+    |wires|: a list of Wire objects to display on this proto board.
+    """
     Frame.__init__(self, parent, background=BACKGROUND_COLOR)
+    parent.title(WINDOW_TITLE)
     self._canvas = Canvas(self, width=WIDTH, height=HEIGHT,
         background=BACKGROUND_COLOR)
     self._wires = wires
     self._set_up()
     self._display_wires()
   def _vertical_section(self, r):
+    """
+    Returns the number of vertical separators that stand between the top of the
+        proto board and the given row |r|.
+    """
     return sum (r >= barrier for barrier in (2, PROTO_BOARD_HEIGHT / 2,
         PROTO_BOARD_HEIGHT - 2))
   def _rc_to_xy(self, r, c):
+    """
+    Returns the top left corner of the connector located at row |r| column |c|.
+    """
     x = c * (CONNECTOR_SIZE + CONNECTOR_SPACING) + PADDING
     y = r * (CONNECTOR_SIZE + CONNECTOR_SPACING) + PADDING + (
         self._vertical_section(r) * (VERTICAL_SEPARATION - CONNECTOR_SPACING))
     return x, y
   def _set_up(self):
+    """
+    Draws the connectors as well as labels for the rows and columns.
+    """
+    # connectors
     for r in ROWS:
       for c in COLUMNS:
         if valid_loc((r, c)):
@@ -50,9 +66,19 @@ class Proto_Board_Visualizer(Frame):
           self._canvas.create_rectangle(x, y, x + CONNECTOR_SIZE,
               y + CONNECTOR_SIZE, fill=CONNECTOR_COLOR,
               outline=CONNECTOR_COLOR)
+    # row labels
+    for r in ROWS:
+      self._canvas.create_text(self._rc_to_xy(r, -1), text=r)
+    # columns labels
+    for c in COLUMNS:
+      if c % 5 == 0:
+        self._canvas.create_text(self._rc_to_xy(-1, c), text=c)
     self._canvas.pack()
     self.pack()
   def _display_wires(self):
+    """
+    Draws the wires on the proto board.
+    """
     for wire in self._wires:
       x_1, y_1 = self._rc_to_xy(wire.r_1, wire.c_1)
       x_2, y_2 = self._rc_to_xy(wire.r_2, wire.c_2)
@@ -61,4 +87,7 @@ class Proto_Board_Visualizer(Frame):
       self._canvas.create_rectangle(x_1, y_1, x_2 + CONNECTOR_SIZE,
           y_2 + CONNECTOR_SIZE, fill=WIRE_COLOR, outline=WIRE_OUTLINE)
   def show(self):
+    """
+    Displays the proto board in a new window.
+    """
     mainloop()
