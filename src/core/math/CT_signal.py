829934f8188707883a1d270d5740ee95cc2da15d
Michael Mekonnen <mikemeko@mit.edu>
2013-04-01 13:08:48 -0400

    function CT signal

--- src/core/math/CT_signal.py
+++ src/core/math/CT_signal.py
@@ -8,6 +8,7 @@ class CT_Signal:
   """
   Representation for CT signals.
   """
+  # TODO(mikemeko): means of combination
   @property
   def sample(t):
     """
@@ -15,3 +16,16 @@ class CT_Signal:
         should implement this method.
     """
     raise NotImplementedError('subclasses should implement this.')
+
+class Function_CT_Signal:
+  """
+  CT signal with samples based on an underlying function.
+  """
+  def __init__(self, f):
+    """
+    |f|: function used to produce samples.
+    """
+    CT_Signal.__init__(self)
+    self.f = f
+  def sample(self, t):
+    return self.f(t)
