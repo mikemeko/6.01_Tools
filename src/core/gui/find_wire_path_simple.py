353ae928f1ab3d31dcfbd87d893c0bb8913a7d92
Michael Mekonnen <mikemeko@mit.edu>
2013-07-29 17:21:39 -0400

    better gui board wiring options

--- src/core/gui/find_wire_path_simple.py
+++ src/core/gui/find_wire_path_simple.py
@@ -5,6 +5,7 @@ Script to find a good path of wires from one point on a board to another,
 
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
+from constants import BOARD_GRID_SEPARATION
 from find_wire_path import wire_coverage
 from find_wire_path import get_board_coverage
 
@@ -28,6 +29,14 @@ def find_wire_path(board, start_point, end_point):
     return [start_point, end_point]
   else:
     paths = [[(x1, y1), (x1, y2), (x2, y2)], [(x1, y1), (x2, y1), (x2, y2)]]
+    x_sign = 1 if x1 <= x2 else -1
+    for x in xrange(x1 + x_sign * BOARD_GRID_SEPARATION, x2, x_sign *
+        BOARD_GRID_SEPARATION):
+      paths.append([(x1, y1), (x, y1), (x, y2), (x2, y2)])
+    y_sign = 1 if y1 <= y2 else -1
+    for y in xrange(y1 + y_sign * BOARD_GRID_SEPARATION, y2, y_sign *
+        BOARD_GRID_SEPARATION):
+      paths.append([(x1, y1), (x1, y), (x2, y), (x2, y2)])
     board_coverage = get_board_coverage(board)
     return min(paths, key=lambda path: len(board_coverage & path_coverage(
         path)))
