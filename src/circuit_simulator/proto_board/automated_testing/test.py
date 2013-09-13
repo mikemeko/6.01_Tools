a51e720e9fedc3a3322e60e315f9690a3ec568cf
Michael Mekonnen <mikemeko@mit.edu>
2013-09-13 13:42:13 -0400

    add more test schematics; better feedback from test script

--- src/circuit_simulator/proto_board/automated_testing/test.py
+++ src/circuit_simulator/proto_board/automated_testing/test.py
@@ -10,15 +10,21 @@ from os import walk
 from os.path import join
 from sys import argv
 from test_schematic import test_schematic
+from time import time
 
 if __name__ == '__main__':
   assert len(argv) == 2
+  start_time = time()
   results = []
   for dir_path, dir_names, file_names in walk(argv[1]):
     for file_name in file_names:
       if file_name.endswith(FILE_EXTENSION):
-        success, time = test_schematic(join(dir_path, file_name))
-        results.append((file_name, success, time))
+        print file_name
+        success, run_time = test_schematic(join(dir_path, file_name))
+        results.append((file_name, success, run_time))
+        print
+  stop_time = time()
+  print 'Time elapsed: %.3f seconds' % (stop_time - start_time)
   pyplot.bar(left=range(len(results)), height=[t for f, s, t in results],
       color=['green' if s else 'red' for f, s, t in results])
   pyplot.xlabel('Test schematics')
