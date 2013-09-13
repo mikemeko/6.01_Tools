55ec1b5d0126d89f15b47bd29fc99d4d37347b13
Michael Mekonnen <mikemeko@mit.edu>
2013-09-13 12:00:00 -0400

    plot for automated test

--- src/circuit_simulator/proto_board/automated_testing/test.py
+++ src/circuit_simulator/proto_board/automated_testing/test.py
@@ -5,6 +5,7 @@ Script that runs automated layout test on a directory of test schematic files.
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
 from circuit_simulator.main.constants import FILE_EXTENSION
+from matplotlib import pyplot
 from os import walk
 from os.path import join
 from sys import argv
@@ -18,4 +19,8 @@ if __name__ == '__main__':
       if file_name.endswith(FILE_EXTENSION):
         success, time = test_schematic(join(dir_path, file_name))
         results.append((file_name, success, time))
-  print '\n'.join(map(str, results))
+  pyplot.bar(left=range(len(results)), height=[t for f, s, t in results],
+      color=['green' if s else 'red' for f, s, t in results])
+  pyplot.xlabel('Test schematics')
+  pyplot.ylabel('Time (seconds)')
+  pyplot.show()
