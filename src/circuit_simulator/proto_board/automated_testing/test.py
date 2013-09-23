881d7e1802cf61c0d528598dc43382080d318c0b
Michael Mekonnen <mikemeko@mit.edu>
2013-09-23 00:26:17 -0400

    write results to file after each schematic is processed instead of waiting until the very end

--- src/circuit_simulator/proto_board/automated_testing/test.py
+++ src/circuit_simulator/proto_board/automated_testing/test.py
@@ -17,20 +17,25 @@ from time import time
 if __name__ == '__main__':
   assert len(argv) == 2
   start_time = time()
-  results = [('file', 'solved', 'solve_time', 'num_resistors', 'num_pots',
+  header = ('file', 'solved', 'solve_time', 'num_resistors', 'num_pots',
       'num_op_amps', 'num_op_amp_packages', 'num_motors', 'head_present',
       'robot_present', 'num_wires', 'total_wire_length', 'num_wire_crosses',
-      'num_nodes')]
+      'num_nodes')
+  output_file_name = ('circuit_simulator/proto_board/automated_testing/'
+      'test_results/%s_%s' % (basename(normpath(argv[1])),
+      datetime.now().strftime('%d%b%I:%M%p')))
+  open(output_file_name, 'w').write(','.join(header))
   for dir_path, dir_names, file_names in walk(argv[1]):
     num_files = len(file_names)
     for n, file_name in enumerate(file_names):
       print '%d/%d' % (n + 1, num_files)
       if file_name.endswith(FILE_EXTENSION):
         print file_name
-        results.append((file_name,) + test_schematic(join(dir_path, file_name)))
+        result = (file_name,) + test_schematic(join(dir_path, file_name))
+        results = [line.strip() for line in open(output_file_name,
+            'r').readlines()]
+        results.append(','.join(map(str, result)))
+        open(output_file_name, 'w').write('\n'.join(results))
         print
-  open('circuit_simulator/proto_board/automated_testing/test_results/%s_%s' %
-      (basename(normpath(argv[1])), datetime.now().strftime('%d%b%I:%M%p')),
-      'w').write('\n'.join(['\t'.join(map(str, row)) for row in results]))
   stop_time = time()
   print 'Time elapsed: %.3f seconds' % (stop_time - start_time)
