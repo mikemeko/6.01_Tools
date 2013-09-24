c7e2501dc4bf0effed99b8414863a64cdd269096
Michael Mekonnen <mikemeko@mit.edu>
2013-09-24 01:27:53 -0400

    additional data analysis code

--- src/circuit_simulator/proto_board/automated_testing/test_results/analyze_data.py
+++ src/circuit_simulator/proto_board/automated_testing/test_results/analyze_data.py
@@ -5,7 +5,11 @@ Script to analyze test data.
 __author__ = 'mikemeko@mit.edu (Michael Mekonnen)'
 
 from collections import defaultdict
-from matplotlib import pyplot as plt
+from math import ceil
+from numpy import mean
+from numpy import std
+from pylab import imshow
+from pylab import show
 from sys import argv
 
 def sequence_generator():
@@ -43,6 +47,9 @@ class Test_Result:
     self.num_motors = get_int(line[g.next()])
     self.head_present = int(line[g.next()] == 'True')
     self.robot_present = int(line[g.next()] == 'True')
+    self.num_components = (self.num_resistors + self.num_pots +
+        self.num_op_amps + self.num_motors + self.head_present +
+        self.robot_present)
     self.num_wires = get_int(line[g.next()])
     self.total_wire_length = get_int(line[g.next()])
     self.num_wire_crosses = get_int(line[g.next()])
@@ -53,19 +60,22 @@ def compare(solved, unsolved, attr):
   Compares the various values of |attr| in the |solved| versus |unsolved|
       schematics.
   """
-  num_solved = defaultdict(int)
+  solved_dict = defaultdict(list)
   for result in solved:
-    num_solved[getattr(result, attr)] += 1
-  num_unsolved = defaultdict(int)
+    solved_dict[getattr(result, attr)].append(result)
+  unsolved_dict = defaultdict(list)
   for result in unsolved:
-    num_unsolved[getattr(result, attr)] += 1
-  keys = sorted(set(num_solved.keys() + num_unsolved.keys()))
-  fig, ax = plt.subplots()
-  width = 0.35
-  rects1 = ax.bar(keys, [num_solved[key] for key in keys], width, color='g')
-  rects2 = ax.bar([key + width for key in keys], [num_unsolved[key] for key in
-      keys], width, color='r')
-  ax.set_title(attr)
+    unsolved_dict[getattr(result, attr)].append(result)
+  print attr
+  print '# #s \% _s _f'
+  keys = sorted(set(solved_dict.keys() + unsolved_dict.keys()))
+  for key in keys:
+    key_total = len(solved_dict[key]) + len(unsolved_dict[key])
+    print ('{0:d} {1:d} {2:.2f} '
+        '{3:.2f} {4:.2f}'.format(key,
+        key_total, float(len(solved_dict[key])) / key_total, mean([
+        result.solve_time for result in solved_dict[key]]), mean([
+        result.solve_time for result in unsolved_dict[key]])))
 
 def analyze(results_file):
   """
@@ -75,15 +85,51 @@ def analyze(results_file):
   results = [Test_Result(line) for line in lines[1:]]
   solved = [result for result in results if result.solved]
   unsolved = [result for result in results if not result.solved]
-  print 'Success: {0:.2f}%'.format(100 * (float(len(solved)) / len(results)))
+  print 'Success rate: {0:.2f}%'.format((100. * len(solved) / len(results)))
+  print 'Solve time: {0:.2f}, {1:.2f}'.format(mean([result.solve_time for result
+      in solved]), std([result.solve_time for result in solved]))
+  print 'Failure time: {0:.2f}, {1:.2f}'.format(mean([result.solve_time for
+      result in unsolved]), std([result.solve_time for result in unsolved]))
+  print 'Num wires: {0:.2f}, {1:.2f}'.format(mean([result.num_wires for result
+      in solved]), std([result.num_wires for result in solved]))
+  print 'Total wire length: {0:.2f}, {1:.2f}'.format(mean([
+      result.total_wire_length for result in solved]), std([
+      result.total_wire_length for result in solved]))
+  print 'Num wire crosses: {0:.2f}, {1:.2f}'.format(mean([
+      result.num_wire_crosses for result in solved]), std([
+      result.num_wire_crosses for result in solved]))
+  print
   compare(solved, unsolved, 'num_resistors')
+  print
   compare(solved, unsolved, 'num_pots')
+  print
   compare(solved, unsolved, 'num_op_amps')
+  print
   compare(solved, unsolved, 'num_motors')
+  print
   compare(solved, unsolved, 'head_present')
+  print
   compare(solved, unsolved, 'robot_present')
+  print
+  compare(solved, unsolved, 'num_components')
+  print
   compare(solved, unsolved, 'num_nodes')
-  plt.show()
+  size = int(ceil(len(results) ** 0.5))
+  incr = sign = 1
+  while len(results) % size != 0:
+    size += incr * sign
+    if sign == -1:
+      incr += 1
+    sign *= -1
+  for k in range(size, -1, -1):
+    if len(results) % k == 0:
+      size = k
+      break
+  grid = [[0] * size for i in xrange(len(results) / size)]
+  for i, result in enumerate(results):
+    grid[i / size][i % size] = result.solved
+  imshow(grid, interpolation='nearest')
+  show()
 
 if __name__ == '__main__':
   assert len(argv) == 2, 'No input'
